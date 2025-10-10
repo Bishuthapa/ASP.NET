4f6e4431574d01c4b5baa4fc708c8ccfc1c75adf
Bishu Thapa <thapabishu435@gmail.com>
2025-10-10 08:28:09 +0545

    inital commit

---
+++ Views/Home/Project.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Home
+{
+    public class ProjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
